27b37dc621d9b1a9608a5de385d7dbc3a3ebe0bb
Barri <info@bdesignstudio.co.uk>
2020-08-29 19:02:01 +0100

    reuse the code

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,8 @@
 import React, { useState } from 'react';
-import ResourceList from './ResourceList'
+import ResourceList from './ResourceList';
+
+import UsersList from './usersResources';
+
 
 const App =()=> {
 // array destructuring --resource is like state, setRecourse is like setState-resource = 'post' or default state 
@@ -7,9 +10,12 @@ const [resource , setResource ] = useState('posts');
  
     return (
       <div className="ui container">
+        <UsersList/>
         <div>
           <button onClick={()=>setResource('Posts')}>Posts</button>
           <button onClick={()=>setResource('Todos')}>Todos</button>
+         
+       
         </div>
         <ResourceList resource = { resource}/>
       </div>
